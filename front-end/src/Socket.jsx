3c5b8e487079c1c318293f75a6802dc0c1852c40
Mykolas Molis <>
2023-03-24 11:15:55 +0200

    Support socket status

--- front-end/src/Socket.jsx
+++ front-end/src/Socket.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default () => {
   const [isPlugged, setIsPlugged] = useState(false);
+  const [isCharging, setIsCharging] = useState(false);
+
+  const plugin = () => {
+    setIsPlugged(true);
+    setIsCharging(true);
+  };
+
+  const unplug = () => {
+    setIsPlugged(false);
+  };
+
+  useEffect(() => {
+    if (isCharging && !isPlugged) {
+      setIsCharging(false);
+    }
+  }, [isPlugged]);
+
   return (
     <>
       <h2>Socket</h2>
-      <button disabled={isPlugged} onClick={() => setIsPlugged(true)}>
-        Plug in
+      <div>Status: {isCharging ? "Charging" : "NOT charging"}</div>
+      <button disabled={isPlugged} onClick={() => plugin()}>
+        Plugin
       </button>
-      <button disabled={!isPlugged} onClick={() => setIsPlugged(false)}>
+      <button disabled={!isPlugged} onClick={() => unplug()}>
         Unplug
       </button>
     </>
